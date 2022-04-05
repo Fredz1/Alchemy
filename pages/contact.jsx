fc377fa514df05d796863ec07e9ea84f7d89f0b2
Fred <fredwil25@gmail.com>
2022-04-05 22:12:34 +0200

    resizing for tablet

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,5 +1,6 @@
 // modules
 import React from 'react'
+import { useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 
@@ -11,59 +12,88 @@ import facebookIcon from '../public/assets/icons/socialMediaIcons/facebookSocial
 import linkedInIcon from '../public/assets/icons/socialMediaIcons/linkedinSocialMediaColor.png'
 
 const contact = () => {
+
+  // Input form State
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phoneNum, setPhoneNum] = useState('')
+  const [message, setMessage] = useState('')
+
+  const sendMessage = () => {
+    let formData = {
+      name,
+      email,
+      phoneNum,
+      message
+    }
+
+    console.log(formData)
+  }
+
   return (
     <div className={style.container}>
-      <h2>
-        Contact Us
-      </h2> 
-      <div>
-        <h3>
-          Office: 
-        </h3>
-        <p>
-          <a href="tel:+27 21 279 1279">021 276 1279</a>
-        </p>
-      </div>
-      <div>
-        <h3>
-          Email
-        </h3>
-        <p>
-          <a href="mailto:alchemy@alchemy.co.za?subject=Im%20interested%20in&body=I%20would%20like%20you%20to%20contact%20me.">alchemy@alchemy.co.za</a>
-        </p>
-      </div>
+      <div className={style.details}>
+        
+        
+        <div>
+          <h3>
+            Office: 
+          </h3>
+          <p>
+            <a href="tel:+27 21 279 1279">021 276 1279</a>
+          </p>
+        </div>
+        <div>
+          <h3>
+            Email
+          </h3>
+          <p>
+            <a href="mailto:alchemy@alchemy.co.za?subject=Im%20interested%20in&body=I%20would%20like%20you%20to%20contact%20me.">alchemy@alchemy.co.za</a>
+          </p>
+        </div>
+      
 
-      <div>
-        <h3>
-          Social Media
-        </h3>
-        <Link href="https://www.facebook.com/fredwil25/" _blank>
-          <a>
-            <Image src={facebookIcon}/>
-          </a>
-        </Link>
-        <Link href="https://www.linkedin.com/in/frederick-williams-b2215730/">
-          <a>
-            <Image src={linkedInIcon}/>
-          </a>
-        </Link>
-        <Link href="https://www.instagram.com/fredimpossibleyt/">
-          <a>
-            <Image src={linkedInIcon}/>
-          </a>
-        </Link>
+        <div>
+          <h3>
+            Social Media
+          </h3>
+          <Link href="https://www.facebook.com/fredwil25/" _blank>
+            <a>
+              <Image src={facebookIcon}/>
+            </a>
+          </Link>
+          <Link href="https://www.linkedin.com/in/frederick-williams-b2215730/">
+            <a>
+              <Image src={linkedInIcon}/>
+            </a>
+          </Link>
+          <Link href="https://www.instagram.com/fredimpossibleyt/">
+            <a>
+              <Image src={linkedInIcon}/>
+            </a>
+          </Link>
+        </div>
       </div>
+      
 
       <div className={style.form}>
-        <h3>
-          Enter your details
-        </h3>
-        <input type="text" placeholder='You full name'/>
-        <input type="email" placeholder='your@email.com'/>
-        <input type="tel" placeholder='555 555 5555'/>
-        <input type="textarea" placeholder='Write your message here'/>
-        <button type="submit">Submit</button>
+        <div>
+          <h3>
+            Enter your details
+          </h3> 
+        </div>
+        <div>
+          <input type="text" placeholder='You full name' value={name} onChange={e => setName(e.target.value)}/>
+          <input type="email" placeholder='your@email.com' value={email} onChange={e => setEmail(e.target.value)}/>
+          <input type="tel" placeholder='555 555 5555' value={phoneNum} onChange={e => setPhoneNum(e.target.value)}/>
+        </div>
+        <input type="textarea" placeholder='Write your message here' value={message} onChange={e => setMessage(e.target.value)}/>
+        
+        
+        <button type="submit" onClick={() => sendMessage()}>Submit</button>
+
       </div>
+
     </div>
   )
 }
